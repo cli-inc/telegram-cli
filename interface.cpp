29651654e3b49ae9d5f2d7f862111afad3c189c5
Finn Behrens <Kloenk@gmx.de>
2019-02-10 00:37:46 +0100

    try to create a string spliter

--- interface.cpp
+++ interface.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <readline/history.h>
 #include <readline/readline.h>
+#include <string>
 
 #include "interface.h"
 #include "colors.h"
@@ -17,36 +18,76 @@ char *commands[] = {
 
 int terminal() {
     rl_attempted_completion_function = command_completion;
+    cmd_helpers::split_str((char*)"Wtf?");
      while (!quit) {  // run until quit
         char *line = readline(inputChar);
-        int code;    // stores exit code of underlying function
         if(!line) break;  // ignore empty lines
         if(*line) add_history(line); // add line to history
-        #ifdef DEBUG    // only print debug if in debug mode compiled
+        //#ifdef DEBUG    // only print debug if in debug mode compiled
         puts(line);
-        #endif
-        if ((strncmp(line, "quit", 4) == 0) || (strncmp(line, "exit", 4) == 0)) {
-            return 0;
-        } else if (strncmp(line, "help", 4) == 0) {
-            code = cmdHelp();
-        }
+        //#endif
         
+        int code = cmd_helpers::execute_command(line);
         if (code != 0) {
             return code;
+        }
+        if (quit) {
+            return 0;
         }
         free(line);
       }
       return -1;
 }
 
-#pragma region cmd_helpers
-int cmdHelp() {
-    printf ("%sexit/quit     quits programm%s\n", COLOR_YELLOW, COLOR_NORMAL);
-    printf ("%shelp          prints this message%s\n", COLOR_YELLOW, COLOR_NORMAL);
-    return 0;
-}
+namespace cmd_helpers {
+    int execute_command(const char *command) {
+        split_str(command);
+        if ((strncmp(command, "quit", 4) == 0) || (strncmp(command, "exit", 4) == 0)) {
+            quit = true;
+            return 0;
+        } else if (strncmp(command, "help", 4) == 0) {
+            return cmd_help();
+        } else if (strncmp(command, "alias", 5) == 0) {
+            return store_alias(command);
+        }
+        return 0;
+    }
 
-#pragma endregion cmd_helpers
+    int store_alias(const char* command) {
+
+        return 0;
+    }
+
+    void split_str(const char* line) {
+        bool first_space = true;
+        int size = sizeof(line);
+
+        std::string main_command_string;
+        std::string args_string;
+        for (int i = 0; i < strlen(line); ++i) {
+            if (line[i] == ' ') {
+                if (!first_space) {
+                    args_string += ' ';
+                }
+                first_space = false;
+            } else {
+                if (first_space) {
+                    main_command_string += line[i];
+                } else {
+                    args_string += line[i];
+                }
+            }
+        }
+        main_command = (char*)main_command_string.c_str();
+        args = (char*)args_string.c_str();
+    }
+
+    int cmd_help() {
+        printf ("%sexit/quit     quits programm%s\n", COLOR_YELLOW, COLOR_NORMAL);
+        printf ("%shelp          prints this message%s\n", COLOR_YELLOW, COLOR_NORMAL);
+        return 0;
+    }
+}
 
 char **command_completion(const char *text, int start, int end) {
     rl_attempted_completion_over = 1;
--- interface.h
+++ interface.h
@@ -1,15 +1,37 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
+#include <map>
+#include <vector>
+
 #define inputChar "> "
 
 /* main run function */
 int terminal();
 
-#pragma region cmd_helpers
-/* cmdHelp print function usable in command line */
-int cmdHelp();
-#pragma endregion cmd_helpers
+namespace cmd_helpers
+{
+    /* current main command contains the current first command */
+    static char* main_command;
+
+    static char* args;
+
+    /* execute_command is a helper to analyze a command string and execute the command */
+    int execute_command(const char *command);
+
+    /* store_alias stores an alias for later use */
+    int store_alias(const char* command);
+
+    /* cmdHelp print function usable in command line */
+    int cmd_help();
+
+    /* split_str splits a string at ' ' */
+    void split_str(const char* line);
+
+    /* aliases holds a list of all aliases */
+    static std::map<char*, char*> aliases;
+} // cmd_helpers
+
 
 // function for autocompletion
 char **command_completion(const char *, int, int);
